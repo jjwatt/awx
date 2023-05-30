f78054cc4602b9c07ca90eca678033c2409cabaf
Jesse Wattenbarger <jwattenb@redhat.com>
2023-05-30 09:01:19 -0400

    Fixups for start_tests.sh

--- tools/docker-compose/start_tests.sh
+++ tools/docker-compose/start_tests.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set +x
 
-cd /awx_devel
+# Get AWX_DEVEL_ROOT from the environment
+# or use the default.
+# TODO: Use the same var name that is usually used for this
+: "${AWX_DEVEL_ROOT:-/awx_devel}"
+{ cd "${AWX_DEVEL_ROOT}" || return; }
 make clean
 make awx-link
 
